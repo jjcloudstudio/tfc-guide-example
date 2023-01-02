0926ac2fcc3ea889247472707ca631e2fafd28b1
JJ <35040051+jjcloudstudio@users.noreply.github.com>
2023-01-02 22:54:13 +0530

    added aws security grp

--- main.tf
+++ main.tf
@@ -18,11 +18,54 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "ubuntu_test" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  vpc_security_groups_ids =[aws_security_group.ubuntu_test.id]
+
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_security_group" "ubuntu_test" {
+  name        = ubuntu_test
+  description = "Allow http and https in. Allow everything out"
+
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "ubuntu_test_http_in" {
+  type        = ingree
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cide_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws.security_group.ubuntu_test.id
+}
+
+resource "aws_security_group_rule" "ubuntu_test_https_in" {
+  type        = ingree
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cide_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws.security_group.ubuntu_test.id
+}
+
+resource "aws_security_group_rule" "ubuntu_test_http_in" {
+  type        = ingree
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cide_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws.security_group.ubuntu_test.id
 }
