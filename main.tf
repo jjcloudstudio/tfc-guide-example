be73b2d0ec235d2e2bf014b5fd56d7080d1f6c7c
JJ <35040051+jjcloudstudio@users.noreply.github.com>
2023-01-08 01:11:51 +0530

    ubuntu_test to blog

--- main.tf
+++ main.tf
@@ -22,11 +22,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "ubuntu_test" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[module.ubuntu_test_sg.security_group_id]
+  vpc_security_group_ids =[module.blog_sg.security_group_id]
 
   tags = {
     Name = var.instance_name
@@ -34,10 +34,10 @@ resource "aws_instance" "ubuntu_test" {
 }
 
 
-module "ubuntu_test_sg" {
+module "blog_sg" {
   source = "terraform-aws-modules/aws_security_group/aws"
   version = "4.13.0"
-  name = "ubuntu_test_sg"
+  name = "blog_sg"
 
   vpc_id = data.aws_vpc.default.id
 
@@ -48,39 +48,39 @@ module "ubuntu_test_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "ubuntu_test" {
-  name        = "ubuntu_test"
+resource "aws_security_group" "blog" {
+  name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
   vpc_id      = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "ubuntu_test_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.ubuntu_test.id
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "ubuntu_test_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.ubuntu_test.id
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "ubuntu_test_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.ubuntu_test.id
+  security_group_id = aws_security_group.blog.id
 }
